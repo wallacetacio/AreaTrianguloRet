46f19e572911acca1001978ceeb65d5376ca2c25
Wallace Tácio <wallacett.dev@gmail.com>
2021-09-14 23:07:37 -0300

    Adiciona resolução ao exercicio Área Triangulo Reto ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bas,altura;
+            Console.WriteLine(" ");
+            Console.WriteLine(@"Olá usuário, seja bem vindo!
+Esse programa irá calcular a área de um triangulo reto com as medidas que você fornecer!");
+            
+            Console.Write("Valor da base: ");
+            bas = Console.ReadLine();
+            
+            Console.Write("Valor da altura: ");
+            altura = Console.ReadLine();
+
+            double b = Convert.ToDouble(bas);
+            double h = Convert.ToDouble(altura);
+
+            Console.WriteLine($"Área: {b * h / 2}");
+
+            Console.WriteLine(@"Espero que tenha gostado! 
+Até Mais!");
+
+        }
+    }
+}
